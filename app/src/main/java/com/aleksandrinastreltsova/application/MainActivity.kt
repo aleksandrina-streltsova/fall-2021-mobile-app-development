6282641fe156cbc5ee93cea089ec48af5f44aab4
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-09-22 17:45:45 +0300

    Add view model

--- app/src/main/java/com/aleksandrinastreltsova/application/MainActivity.kt
+++ app/src/main/java/com/aleksandrinastreltsova/application/MainActivity.kt
@@ -1,60 +1,60 @@
- package com.aleksandrinastreltsova.application
+package com.aleksandrinastreltsova.application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.ProgressBar
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.lifecycle.withCreated
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.moshi.Moshi
-import kotlinx.coroutines.Dispatchers
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.aleksandrinastreltsova.application.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
- class MainActivity : AppCompatActivity(R.layout.activity_main) {
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
-         val adapter = UserAdapter()
-         recyclerView.adapter = adapter
-         lifecycleScope.launch {
-             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                 adapter.userList = loadUsers()
-                 adapter.notifyDataSetChanged()
-             }
-         }
-     }
+class MainActivity : AppCompatActivity() {
 
-     private suspend fun loadUsers(): List<User> {
-         return withContext(Dispatchers.IO) {
-             Thread.sleep(3000)
-             provideApi().getUsers().data
-         }
-     }
+    private val viewModel: MainViewModel by viewModels()
+    private val viewBinding by viewBinding(ActivityMainBinding::bind)
 
-     private fun provideApi(): Api {
-         return Retrofit.Builder()
-             .client(provideOkHttpClient())
-             .baseUrl("https://reqres.in/api/")
-             .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
-             .build()
-             .create(Api::class.java)
-     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.viewState.collect { viewState ->
+                    renderViewState(viewState)
+                }
+            }
+        }
+    }
 
-     private fun provideOkHttpClient(): OkHttpClient {
-         return OkHttpClient.Builder().build()
-     }
+    private fun renderViewState(viewState: MainViewModel.ViewState) {
+        when (viewState) {
+            is MainViewModel.ViewState.Loading -> {
+                viewBinding.usersRecyclerView.isVisible = false
+                viewBinding.progressBar.isVisible = true
 
-     private fun provideMoshi(): Moshi {
-         return Moshi.Builder().build()
-     }
+            }
+            is MainViewModel.ViewState.Data -> {
+                viewBinding.usersRecyclerView.isVisible = true
+                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
+                    userList = viewState.userList
+                    notifyDataSetChanged()
+                }
+                viewBinding.progressBar.isVisible = false
+
+            }
+        }
+    }
+
+    private fun setupRecyclerView(): UserAdapter {
+        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        return adapter
+    }
 
 }
