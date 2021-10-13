26103ab3f8b8830fb0c00cb2ee2b7fdbc57c6479
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-10-13 12:49:02 +0300

    Add text to onboarding fragment

---
+++ app/src/main/java/com/aleksandrinastreltsova/application/UserListFragment.kt
@@ -0,0 +1,56 @@
+package com.aleksandrinastreltsova.application
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.aleksandrinastreltsova.application.databinding.FragmentUserListBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class UserListFragment : BaseFragment(R.layout.fragment_user_list) {
+
+    private val viewModel: UserListViewModel by viewModels()
+    private val viewBinding by viewBinding(FragmentUserListBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupRecyclerView()
+        subscribeToViewState()
+    }
+
+    private fun subscribeToViewState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.viewState.collect { viewState -> renderViewState(viewState) }
+            }
+        }
+    }
+
+    private fun renderViewState(viewState: UserListViewModel.ViewState) {
+        when (viewState) {
+            is UserListViewModel.ViewState.Loading -> {
+                viewBinding.usersRecyclerView.isVisible = false
+                viewBinding.progressBar.isVisible = true
+
+            }
+            is UserListViewModel.ViewState.Data -> {
+                viewBinding.usersRecyclerView.isVisible = true
+                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
+                    userList = viewState.userList
+                    notifyDataSetChanged()
+                }
+                viewBinding.progressBar.isVisible = false
+
+            }
+        }
+    }
+
+    private fun setupRecyclerView(): UserAdapter = UserAdapter().also {
+        viewBinding.usersRecyclerView.adapter = it
+    }
+}
