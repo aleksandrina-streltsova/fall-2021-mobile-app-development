1cfe1513389fefa04ef7d86aa3ad7c57402e4e6a
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-11-02 21:49:51 +0300

    add current changes

---
+++ app/src/main/java/com/aleksandrinastreltsova/application/repository/AuthRepository.kt
@@ -0,0 +1,19 @@
+package com.aleksandrinastreltsova.application.repository
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+object AuthRepository {
+
+    private val _isAuthorizedFlow = MutableStateFlow(false)
+    val isAuthorizedFlow = _isAuthorizedFlow.asStateFlow()
+
+    suspend fun signIn(email: String, password: String) {
+        _isAuthorizedFlow.emit(true)
+    }
+
+    suspend fun logout() {
+        _isAuthorizedFlow.emit(false)
+    }
+}
--- app/src/main/java/com/aleksandrinastreltsova/application/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/aleksandrinastreltsova/application/ui/signin/SignInFragment.kt
@@ -1,6 +1,11 @@
 package com.aleksandrinastreltsova.application.ui.signin
 
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.aleksandrinastreltsova.application.ui.base.BaseFragment
 import com.aleksandrinastreltsova.application.R
@@ -10,5 +15,57 @@ class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 
     private val viewBinding by viewBinding(FragmentSignInBinding::bind)
     private val viewModel: SignInViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            // TODO: there's also system back button...
+            val email = viewBinding.emailEditText.text?.toString() ?: ""
+            val password = viewBinding.passwordEditText.text?.toString() ?: ""
+            if (email.isBlank() && password.isBlank()) {
+                findNavController().popBackStack() // TODO: hide keyboard
+                return@setOnClickListener
+            }
+            AlertDialog.Builder(requireContext())
+                .setTitle(R.string.sign_in_back_alert_dialog_text)
+                .setNegativeButton(R.string.sign_in_back_alert_dialog_cancel_button_text) { dialog, _ ->
+                    dialog?.dismiss()
+                }
+                .setPositiveButton(R.string.sign_in_back_alert_dialog_ok_button_text) { _, _ ->
+                    findNavController().popBackStack()
+                }
+                .show()
+        }
+        viewBinding.signInButton.setOnClickListener {
+            viewModel.signIn(
+                viewBinding.emailEditText.text?.toString() ?: "",
+                viewBinding.passwordEditText.text?.toString() ?: ""
+            )
+        }
+        subscribeToFormFields()
+    }
+
+    private fun subscribeToFormFields() {
+        updateSignInButtonState(
+            viewBinding.emailEditText.text?.toString(),
+            viewBinding.passwordEditText.text?.toString()
+        )
+        viewBinding.emailEditText.doAfterTextChanged { email ->
+            updateSignInButtonState(
+                email?.toString(),
+                viewBinding.passwordEditText.text?.toString()
+            )
+        }
+        viewBinding.passwordEditText.doAfterTextChanged { password ->
+            updateSignInButtonState(
+                viewBinding.emailEditText.text?.toString(),
+                password?.toString()
+            )
+        }
+    }
+
+    private fun updateSignInButtonState(email: String?, password: String?) {
+        viewBinding.signInButton.isEnabled = !(email.isNullOrBlank() || password.isNullOrBlank())
+    }
 
 }
