f7ecc93cee6663e9aee14e87f11c02f2fddea905
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-11-28 18:02:02 +0300

    Use insetter

--- app/src/main/java/com/aleksandrinastreltsova/application/ui/MainActivity.kt
+++ app/src/main/java/com/aleksandrinastreltsova/application/ui/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         subscribeToAuthorizationStatus()
     }
 
