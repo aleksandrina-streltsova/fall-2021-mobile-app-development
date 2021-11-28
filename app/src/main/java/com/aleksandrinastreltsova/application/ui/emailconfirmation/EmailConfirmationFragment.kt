3226ec09c8401084374434e2f321fa73b2dba510
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-11-28 16:33:59 +0300

    Add fragment with verification code

--- app/src/main/java/com/aleksandrinastreltsova/application/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/aleksandrinastreltsova/application/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,14 +1,24 @@
 package com.aleksandrinastreltsova.application.ui.emailconfirmation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.aleksandrinastreltsova.application.ui.base.BaseFragment
 import com.aleksandrinastreltsova.application.R
 import com.aleksandrinastreltsova.application.databinding.FragmentEmailConfirmationBinding
+import com.aleksandrinastreltsova.application.ui.base.BaseFragment
 
 class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmation) {
 
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
+
     private val viewModel: EmailConfirmationViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
