da110923779fc1ecfc956bea31ba0909aa2063c1
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-12-07 18:21:15 +0300

    Implement post's view

---
+++ app/src/main/java/com/aleksandrinastreltsova/application/util/ContextExtensions.kt
@@ -0,0 +1,7 @@
+package com.aleksandrinastreltsova.application.util
+
+import android.content.Context
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+
+fun Context.getCompatColor(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
